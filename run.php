619a5484ef59d5be2cb14bb51b995e30bced859d
Evinek <evinek95@gmail.com>
2018-05-16 15:22:17 +0200

    Create run.php

---
+++ run.php
@@ -0,0 +1,14 @@
+<?php
+
+include('functions.php');
+
+$hi = fopen('php://stdin', "r");
+$ho = fopen('php://stdout', "w");
+
+fscanf($hi, "%d", $number);
+
+$result = awesome_function($number);
+
+fwrite($ho, sprintf("%d\n", $result));
+
+fclose($ho);
